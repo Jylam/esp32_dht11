36ff52d1c1c5a1dd6cb668d1c02e5d0b3504449c
Jylam <jylam@lnxscene.org>
2018-09-16 00:27:34 +0200

    Use a semaphore to communicate with the ISR

--- main/dht11.c
+++ main/dht11.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_system.h"
 #include "rom/ets_sys.h"
 #include "nvs_flash.h"
@@ -19,30 +20,44 @@
 #define DHT_EDGES_PER_READ (82)
 #define DHT_RESPONSE_TIMEOUT_US 20000
 
-static void IRAM_ATTR dht_isr_handler(void *args);
+static void dht_isr_handler(void *args);
 
 struct {uint64_t ts; int value; }    edges[DHT_EDGES_PER_READ];
+SemaphoreHandle_t message_semaphore = NULL;
+StaticSemaphore_t message_semaphore_buffer;
 int      dht_pin          = -1;
 uint64_t intr_start_time  = 0;
 int      isr_count        = 0;
-int      message_received = 0;
 int      dht_humidity     = -1;
 int      dht_temp         = -1;
 
+uint64_t poll_start, poll_end;
+
 /* Configure GPIO as an output, no interruption */
-static void dht_setup_pin_output() {
+static void IRAM_ATTR dht_setup_pin_output() {
     gpio_config_t gpioConfig;
+
     gpioConfig.pin_bit_mask = 1<<dht_pin;
     gpioConfig.mode         = GPIO_MODE_OUTPUT;
     gpioConfig.pull_up_en   = GPIO_PULLUP_DISABLE;
     gpioConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
     gpioConfig.intr_type    = GPIO_INTR_DISABLE;
+
     gpio_config(&gpioConfig);
 }
 
 /* Configure GPIO for interruptions on both edges */
-static inline int dht_setup_interrupt(void) {
+static inline int IRAM_ATTR dht_setup_interrupt(void) {
     gpio_config_t gpioConfig;
+
+    /* Reset */
+    isr_count        = 0;
+
+    intr_start_time = esp_timer_get_time();
+    gpio_install_isr_service(ESP_INTR_FLAG_EDGE);
+    gpio_isr_handler_add(dht_pin, dht_isr_handler, NULL);
+
+
     gpioConfig.pin_bit_mask = 1<<dht_pin;
     gpioConfig.mode         = GPIO_MODE_INPUT;
     /* Pull-up is not necessary, and will cause a short circuit,
@@ -50,13 +65,6 @@ static inline int dht_setup_interrupt(void) {
     gpioConfig.pull_up_en   = GPIO_PULLUP_DISABLE;
     gpioConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
     gpioConfig.intr_type    = GPIO_INTR_ANYEDGE;
-
-    message_received = 0;
-    isr_count        = 0;
-
-    intr_start_time = esp_timer_get_time();
-    gpio_install_isr_service(ESP_INTR_FLAG_EDGE);
-    gpio_isr_handler_add(dht_pin, dht_isr_handler, NULL);
 
     gpio_config(&gpioConfig);
 
@@ -74,9 +82,10 @@ static void IRAM_ATTR dht_isr_handler(void *args) {
 
     isr_count++;
     if((isr_count >= DHT_EDGES_PER_READ)) {
+            BaseType_t xHigherPriorityTaskWoken = pdFALSE;
             gpio_uninstall_isr_service();
             dht_setup_pin_output();
-            message_received = 1;
+            xSemaphoreGiveFromISR(message_semaphore, &xHigherPriorityTaskWoken);
     }
 }
 
@@ -94,18 +103,17 @@ static uint8_t dht_decode_byte(uint8_t *bits)
     return ret;
 }
 
-
 /* Wakeup the DHT11
  * Configure the GPIO to output, and set it to LOW for 20ms
  * */
-static void dht_send_start()
+static void IRAM_ATTR dht_send_start()
 {
     dht_setup_pin_output();
 
     /* Pull LOW for 20ms */
     gpio_set_level(dht_pin,0);
 	// Should be at least 18
-	vTaskDelay(20 / portTICK_RATE_MS);
+    ets_delay_us(20000);
 
     /* Set pin to HIGH, the DHT11 wakes up here (or ~12us later)*/
     gpio_set_level(dht_pin,1);
@@ -130,32 +138,31 @@ void dht_set_pin(int pin)
 /* Send START, wait for completion, and compute the humidy and temperature */
 int dht_get_data(void) {
     int ret;
+    message_semaphore = xSemaphoreCreateBinaryStatic(&message_semaphore_buffer);
 
     intr_start_time = 0;
     isr_count = 0;
-    message_received = 0;
 
-    memset(edges, 0, DHT_EDGES_PER_READ*sizeof(struct {uint64_t ts; int value; }));
+    memset(edges, 0, DHT_EDGES_PER_READ*sizeof(struct {uint64_t ts; int value;}));
+
     /* Wakeup the device */
     dht_send_start();
     /* Setup GPIO interrupt service */
     dht_setup_interrupt();
 
-    while(!message_received) {
-
-        if((esp_timer_get_time()-intr_start_time)>DHT_RESPONSE_TIMEOUT_US) {
+    if( xSemaphoreTake(message_semaphore, (DHT_RESPONSE_TIMEOUT_US/1000) / portTICK_PERIOD_MS) != pdTRUE) {
             gpio_uninstall_isr_service();
             ret = DHT_TIMEOUT_ERROR;
             printf("Timeout, %d edges\n", isr_count);
+            printf("Error Poll took %lldus\n", poll_end-poll_start);
             goto end;
-        }
-
-        ets_delay_us(1000);
     }
+    printf("Poll took %lldus\n", poll_end-poll_start);
 
+    int i;
     uint8_t bits[40] = {0x00};
     uint8_t  offset = 0;
-    for(int i = 3; i < DHT_EDGES_PER_READ; i++) {
+    for(i = 3; i < DHT_EDGES_PER_READ; i++) {
         uint64_t diff = edges[i].ts-edges[i-1].ts;
         if(  (diff>40) && (diff < 60) ) {
         }
