ea24da47b77829699f203361431737b055a46937
jylam <jylam@lnxscene.org>
2019-05-18 10:24:46 +0200

    Removed DHT11 usage, fixed deprecated headers

--- main/main.c
+++ main/main.c
@@ -7,17 +7,16 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "rom/ets_sys.h"
+#include "esp32/rom/ets_sys.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include <sys/time.h>
 #include "sdkconfig.h"
 #include "esp_http_client.h"
-#include "rom/rtc.h"
+#include "esp32/rom/rtc.h"
 #include "esp_sleep.h"
 
 
-#include "dht11.h"
 #include "wifi.h"
 #include "sntp.h"
 
@@ -35,43 +34,6 @@ uint64_t get_time_us(void) {
     return (sec*1000000)+us;
 }
 
-struct dht_result {
-    int temp;
-    int humi;
-    bool valid;
-};
-
-int temp_done = 0;
-void DHT_task(void *pvParameter) {
-    struct dht_result *res = (struct dht_result*) pvParameter;
-    int avg_temp = 0;
-    int avg_humi = 0;
-    int valid_count = 0;
-
-    res->valid = false;
-
-    // Wait a bit, the DHT needs time to init
-    vTaskDelay(1000 / portTICK_RATE_MS);
-
-    for(int i=0; i<5; i++ ) {
-        if(dht_get_data() == DHT_OK) {
-            int temp = dht_get_temperature();
-            int humi = dht_get_humidity();
-            avg_temp += temp;
-            avg_humi += humi;
-            valid_count+=1;
-        }
-        vTaskDelay(2000 / portTICK_RATE_MS);
-    }
-
-
-    if(valid_count) {
-        res->temp = avg_temp/valid_count;
-        res->humi = avg_humi/valid_count;
-        res->valid = true;
-    }
-    vTaskDelete(NULL);
-}
 
 void SNTP_task(void *ptr) {
     initialize_sntp();
@@ -120,37 +82,20 @@ char *get_unique_id_str(void) {
 
 
 void app_main() {
-    TaskHandle_t dht_handle;
     TaskHandle_t sntp_handle;
-    struct dht_result dht_result;
 
     esp_reset_reason_t reset_reason = rtc_get_reset_reason(0); // 1: power-on    5: deep-sleep
 
 
-    printf("Swarm32 (c) Jylam 2018\n");
+    printf("Swarm32-plants (c) Jylam 2018-2019\n");
     nvs_flash_init();
 
     // Init wifi (returns almost immediately)
     wifi_init();
     // Run SNTP task
     xTaskCreate(&SNTP_task, "SNTP_task", 2048, NULL, configMAX_PRIORITIES, &sntp_handle);
-
-    // Configure DHT
-    dht_set_pin(GPIO_NUM_27);
-    dht_set_type(DHT_TYPE11);
-
-    // Run DHT task
-    xTaskCreate(&DHT_task, "DHT_task", 20480, &dht_result, configMAX_PRIORITIES, &dht_handle);
-    vTaskDelay(100 / portTICK_RATE_MS);
-
-    // Wait for DHT task completion
-    while(eTaskGetState(dht_handle) != eReady) { // ??? should be eDelete ?
-        vTaskDelay(500 / portTICK_RATE_MS);
-    }
-    if(dht_result.valid) {
-        printf("Got measurement : %f %f\n", dht_result.temp/1000.0, dht_result.humi/1000.0);
-    }
-
+    // Wait for the task to be created and SNTP stack to be initialized FIXME
+    vTaskDelay(500 / portTICK_RATE_MS);
     // Wait for SNTP
     int got_sntp = xEventGroupWaitBits(
             sntp_event_group ,
@@ -170,7 +115,7 @@ void app_main() {
         printf("The current date/time is: %s (%d)\n", strftime_buf, (unsigned int)now);
 
         char post_data[256] = "";
-        snprintf(post_data, 256, "http://frob.fr/SWARM32/?id=%s&time=%u&temp=%d&humi=%d&reset=%d", get_unique_id_str() ,(unsigned int)now, dht_result.temp, dht_result.humi, reset_reason);
+        snprintf(post_data, 256, "http://frob.fr/SWARM32/?id=%s&time=%u&gnd_humi=%d&reset=%d", get_unique_id_str() ,(unsigned int)now, -1, reset_reason);
 
         /* Send HTTP message*/
         esp_http_client_config_t config = {
